4aa1e756e8ee8eaf01be34a260908e8278c5aafe
iMuhammadTalha <muhammadtalha3810@gmail.com>
2023-04-11 11:02:45 -0700

    team updated

--- WheatRust/settings.py
+++ WheatRust/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p5s%30gm=-i)snu!kc+*t@%hf0w_m=t0w6-irp6k1^^&j^3)!6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['111.68.101.17']
+ALLOWED_HOSTS = ['111.68.101.17', 'localhost']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
